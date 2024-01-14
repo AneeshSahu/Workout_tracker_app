4c8239a64b197bb2bb497ea43b6393d1fbb62d16
AneeshSahu <aneesh2115@gmail.com>
2024-01-14 02:03:31 -0500

    added button

--- app/src/main/java/com/example/fitness/MainActivity.kt
+++ app/src/main/java/com/example/fitness/MainActivity.kt
@@ -12,20 +12,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.fitness.ui.theme.FitnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +52,7 @@ class MainActivity : ComponentActivity() {
 
                 var reps = mutableListOf<Int>(16,16,16)
 
-                LazyColumn{
+                /*LazyColumn{
                     item(){
                         WorkoutCard(name = "bench", weights = weight, reps = reps)
                     }
@@ -55,8 +61,21 @@ class MainActivity : ComponentActivity() {
                     }
                     item(){
                         WorkoutCard(name = "bench", weights = weight, reps = reps)
+                    }
+                }*/
+                ConstraintLayout (modifier = Modifier.fillMaxSize()){
+                    val button = createRef()
+                    FloatingActionButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.constrainAs(button){
+                            absoluteRight.linkTo(parent.absoluteRight)
+                            bottom.linkTo(parent.bottom)
+                        }
+                    ) {
+                        Icon(Icons.Filled.Add, "Floating action button.")
                     }
                 }
+
 
             }
         }
